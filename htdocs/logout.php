88d41216f957f98bb0a22b1af779df964580fd5c
Deon George <wurley@users.sf.net>
2012-09-03 06:19:19 +1000

    SF Bug #3426575 - clicking 'logout' does not unset _SESSION['ACTIVITY']

--- htdocs/logout.php
+++ htdocs/logout.php
@@ -11,13 +11,16 @@
 
 require './common.php';
 
-if ($app['server']->logout())
+if ($app['server']->logout()) {
+	unset($_SESSION['ACTIVITY'][$app['server']->getIndex()]);
+
 	system_message(array(
-		'title'=>_('Authenticate to server'),
+		'title'=>_('Logout from Server'),
 		'body'=>_('Successfully logged out of server.'),
 		'type'=>'info'),
 		sprintf('index.php?server_id=%s',$app['server']->getIndex()));
-else
+
+} else
 	system_message(array(
 		'title'=>_('Failed to Logout of server'),
 		'body'=>_('Please report this error to the admins.'),
