3ec9c23d585826aa29f5f25ac43ca077f5fe5377
Patrick Monnerat <patrick@monnerat.net>
2022-01-14 01:04:56 +0100

    Do not call get_magic_quote_gpc() when running in PHP version >= 5.4.
    This deprecated function has been removed in PHP 8.

--- lib/common.php
+++ lib/common.php
@@ -297,7 +297,9 @@ if ($app['language'] == 'auto') {
  * Strip slashes from GET, POST, and COOKIE variables if this
  * PHP install is configured to automatically addslashes()
  */
-if (@get_magic_quotes_gpc() && (! isset($slashes_stripped) || ! $slashes_stripped)) {
+if (@version_compare(phpversion(), '5.4.0', '<') &&
+    @get_magic_quotes_gpc() &&
+    (!isset($slashes_stripped) || !$slashes_stripped)) {
 	array_stripslashes($_REQUEST);
 	array_stripslashes($_GET);
 	array_stripslashes($_POST);
