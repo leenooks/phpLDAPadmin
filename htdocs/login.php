d15bd2dab3ec8b01e9725bde1c6bcc531d3d9637
bendem <online+github@bendem.be>
2022-08-18 02:44:43 +0200

    Don't do any decoding on the password field (#163)
    Fixes #162

--- htdocs/login.php
+++ htdocs/login.php
@@ -19,7 +19,7 @@ if ($_SESSION[APPCONFIG]->getValue('session', 'reCAPTCHA-enable')) {
 if ($pass) {
     $user             = array();
     $user['login']    = get_request('login');
-    $user['password'] = get_request('login_pass', 'POST', false, '');
+    $user['password'] = get_request('login_pass', 'POST', false, '', false);
 
     if ($user['login'] && !strlen($user['password'])) {
         system_message(array(
