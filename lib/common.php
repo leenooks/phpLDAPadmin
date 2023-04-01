d1dae60f05d82a12f4631bf764db21c04a2b9a3a
Deon George <deon@dege.au>
2023-04-02 09:10:49 +1000

    get_magic_quotes_gpc() was deprecrated as of PHP 7.4

--- lib/common.php
+++ lib/common.php
@@ -297,7 +297,7 @@ if ($app['language'] == 'auto') {
  * Strip slashes from GET, POST, and COOKIE variables if this
  * PHP install is configured to automatically addslashes()
  */
-if (@get_magic_quotes_gpc() && (! isset($slashes_stripped) || ! $slashes_stripped)) {
+if ((! isset($slashes_stripped) || ! $slashes_stripped)) {
 	array_stripslashes($_REQUEST);
 	array_stripslashes($_GET);
 	array_stripslashes($_POST);
