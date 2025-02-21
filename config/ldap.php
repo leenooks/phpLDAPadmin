1cc8681b5a0260e4614216d6734a978b8901f874
Deon George <deon@dege.au>
2025-02-21 21:42:02 +1100

    Add example ldaps configuration, set TLS_REQCERT to never so php_ldap does validate ldap server SSL certs

--- config/ldap.php
+++ config/ldap.php
@@ -40,6 +40,18 @@ return [
 			'name' => env('LDAP_NAME','LDAP Server'),
 		],
 
+		'openldaps' => [
+			'hosts' => [env('LDAP_HOST', '127.0.0.1')],
+			'username' => env('LDAP_USERNAME', 'cn=user,dc=local,dc=com'),
+			'password' => env('LDAP_PASSWORD', 'secret'),
+			'port' => env('LDAP_PORT', 636),
+			'base_dn' => env('LDAP_BASE_DN', 'dc=local,dc=com'),
+			'timeout' => env('LDAP_TIMEOUT', 5),
+			'use_ssl' => env('LDAP_SSL', true),
+			'use_tls' => env('LDAP_TLS', true),
+			'name' => env('LDAP_NAME','LDAPS Server'),
+		],
+
 		'opendj' => [
 			'hosts' => ['opendj'],
 			'username' => 'cn=Directory Manager',
