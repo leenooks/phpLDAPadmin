ff0bbc758dee3c98bfc3967e499ecc06ca42b0fa
Deon George <deon@dege.au>
2025-02-22 17:32:59 +1100

    Removed some old files where functionality has been adapted in PLA v2

--- htdocs/js/dnChooserPopup.js
+++
@@ -1,6 +0,0 @@
-function dnChooserPopup(form,element,rdn)
-{
-	mywindow=open('entry_chooser.php','myname','resizable=no,width=600,height=370,scrollbars=1');
-	mywindow.location.href = 'entry_chooser.php?form=' + form + '&element=' + element + '&rdn=' + rdn;
-	if (mywindow.opener == null) mywindow.opener = self;
-}
