c8c3939d5919e9cdd9decb2c5745ad740f4ea9c0
Deon George <deon@dege.au>
2025-03-06 12:55:30 +1100

    Style changes when rendering the DN header

--- resources/views/components/attribute/binary/jpegphoto.blade.php
+++ resources/views/components/attribute/binary/jpegphoto.blade.php
@@ -9,12 +9,12 @@
 					@default
 						<td>
 							<input type="hidden" name="{{ $o->name_lc }}[]" value="{{ md5($value) }}">
-							<img @class(['jpegphoto','is-invalid'=>($e=$errors->get($o->name_lc.'.'.$loop->index))]) src="data:{{ $x }};base64, {{ base64_encode($value) }}" />
+							<img @class(['border','rounded','p-2','m-0','is-invalid'=>($e=$errors->get($o->name_lc.'.'.$loop->index))]) src="data:{{ $x }};base64, {{ base64_encode($value) }}" />
 
 							@if ($edit)
 								<br>
 								<!-- @todo TO IMPLEMENT -->
-								<span class="btn btn-sm btn-danger deletable d-none"><i class="fas fa-trash-alt"></i> @lang('Delete')</span>
+								<span class="btn btn-sm btn-danger deletable d-none mt-3"><i class="fas fa-trash-alt"></i> @lang('Delete')</span>
 
 								<div class="invalid-feedback pb-2">
 									@if($e)
--- resources/views/fragment/dn/header.blade.php
+++ resources/views/fragment/dn/header.blade.php
@@ -1,28 +1,28 @@
 <table class="table table-borderless">
-	<tr>
-		<td class="{{ ($x=$o->getObject('jpegphoto')) ? 'border' : '' }}" rowspan="2">
-			{!! $x ? $x->render(FALSE,TRUE) : sprintf('<div class="page-title-icon f32"><i class="%s"></i></div>',$o->icon() ?? "fas fa-info") !!}
+	<tr class="border-bottom line-height-2">
+		<td class="p-1 pt-0" rowspan="2">
+			{!! ($x=$o->getObject('jpegphoto')) ? $x->render(FALSE,TRUE) : sprintf('<div class="page-title-icon f32 m-2"><i class="%s"></i></div>',$o->icon() ?? "fas fa-info") !!}
 		</td>
-		<td class="text-end align-text-top p-0 pt-2 pe-3 {{ $x ? 'ps-5' : '' }}"><strong>{{ $o->getDn() }}</strong></td>
+		<td class="text-end align-bottom pb-0 mb-0 pt-2 pe-3 {{ $x ? 'ps-3' : '' }}"><strong>{{ $o->getDn() }}</strong></td>
 	</tr>
 	<tr>
-		<td class="line-height-1" style="font-size: 55%;vertical-align: bottom;" colspan="2">
-			<table>
-				<tr>
-					<td class="p-1 m-1">Created</td>
-					<th class="p-1 m-1">
+		<td class="align-bottom" style="font-size: 55%" colspan="2">
+			<table class="table table-condensed table-borderless w-100">
+				<tr class="mt-1">
+					<td class="p-0 pe-2">Created</td>
+					<th class="p-0">
 						<x-attribute :o="$o->getObject('createtimestamp')" :na="__('Unknown')"/> [<x-attribute :o="$o->getObject('creatorsname')" :na="__('Unknown')"/>]
 					</th>
 				</tr>
-				<tr>
-					<td class="p-1 m-1">Modified</td>
-					<th class="p-1 m-1">
+				<tr class="mt-1">
+					<td class="p-0 pe-2">Modified</td>
+					<th class="p-0">
 						<x-attribute :o="$o->getObject('modifytimestamp')" :na="__('Unknown')"/> [<x-attribute :o="$o->getObject('modifiersname')" :na="__('Unknown')"/>]
 					</th>
 				</tr>
-				<tr>
-					<td class="p-1 m-1">UUID</td>
-					<th class="p-1 m-1">
+				<tr class="mt-1">
+					<td class="p-0 pe-2">UUID</td>
+					<th class="p-0">
 						<x-attribute :o="$o->getObject('entryuuid')" :na="__('Unknown')"/>
 					</th>
 				</tr>
