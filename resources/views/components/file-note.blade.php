e99e349c0b78a9c680394e5b107e0f407b7f2143
Deon George <deon@dege.au>
2025-03-07 13:32:09 +1100

    Make the file-note responsive to screen size, with a more appropriate size

--- resources/views/components/file-note.blade.php
+++ resources/views/components/file-note.blade.php
@@ -1,7 +1,7 @@
 @if(file_exists($file))
 	<div class="row pb-3">
-		<div class="col-12">
-			<div class="mx-auto card text-white card-body bg-primary w-50">
+		<div class="col-12 offset-md-2 col-md-8">
+			<div class="mx-auto card text-white card-body bg-primary">
 				<h5 class="text-white card-title"><i class="icon fa-2x fas fa-info pe-3"></i><span class="font-size-xlg">NOTE</span></h5>
 				<span class="w-100 pb-0">
 					{!! file_get_contents($file) !!}
