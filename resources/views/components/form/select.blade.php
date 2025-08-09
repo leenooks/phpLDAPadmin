d382e3821afa4f2728d27d579a63cfbeebe4c207
Deon George <deon@dege.au>
2025-08-09 14:24:17 +1000

    Fix another regression as a result of 7cf81c14d - form labels were not being rendered

--- resources/views/components/form/select.blade.php
+++ resources/views/components/form/select.blade.php
@@ -2,6 +2,24 @@
 	<input type="hidden" id="{{ $id ?? $name }}_disabled" name="{{ $name }}" value="" disabled>
 @endisset
 
+@if(isset($label))
+	<label {{ $attributes->only(['class'])->merge(['class'=>'form-label']) }} for="{{ $id ?? $name }}">{!! html_entity_decode($label) !!}</label>
+@endisset
+
 <div class="input-group">
-	<x-select :id="$id ?? NULL" :name="$name ?? NULL" :options="$options ?? []" :value="$value ?? NULL" :class="$class ?? NULL"/>
+	<x-select :id="$id ?? NULL" :name="$name ?? NULL" :old="$old ?? NULL" :options="$options ?? []" :value="$value ?? NULL" :class="$class ?? NULL"/>
+
+	@isset($name)
+		<span class="invalid-feedback">
+			@error((! empty($old)) ? $old : ($id ?? $name))
+				{{ $message }}
+			@elseif(isset($feedback))
+				{{ $feedback }}
+			@enderror
+		</span>
+	@endisset
+
+	@isset($helper)
+		<span class="input-helper">{!! html_entity_decode($helper) !!}</span>
+	@endif
 </div>
