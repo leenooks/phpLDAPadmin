4cb5be8d80ba9dda1fe50335c89a3b72fce4b27e
Deon George <deon@dege.au>
2025-09-13 11:40:34 +1000

    Fix 192.168.0.0 is a 16 bit class C non-routable address

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -27,7 +27,7 @@ return Application::configure(basePath: dirname(__DIR__))
 			'10.0.0.0/8',
 			'127.0.0.0/8',
 			'172.16.0.0/12',
-			'192.168.0.0/12',
+			'192.168.0.0/16',
 		]);
 	})
 	->withExceptions(function (Exceptions $exceptions) {
