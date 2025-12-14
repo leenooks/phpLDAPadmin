4732df39009e96f29b0dc484a25b65671c031dd3
Deon George <deon@dege.au>
2025-12-14 21:49:06 +1100

    Additional fix for 25b0892 to ensure existing entry's template variables are sorted as per the template

--- resources/views/fragment/template/attrs.blade.php
+++ resources/views/fragment/template/attrs.blade.php
@@ -1,6 +1,8 @@
 <div class="card-body">
 	<div class="tab-content">
-		@foreach($o->getVisibleAttributes()->filter(fn($item)=>$template->attributes->keys()->map('strtolower')->contains($item->name_lc)) as $ao)
+		@foreach($o->getVisibleAttributes()
+			->filter(fn($item)=>$template->attributes->keys()->map('strtolower')->contains($item->name_lc))
+			->sortBy(fn($item)=>$template->order->get($item->name_lc)) as $ao)
 			<x-attribute :o="$ao" :edit="false" :editable="false" :new="false" :template="$template" :updated="$updated->contains($ao->name)"/>
 		@endforeach
 	</div>
