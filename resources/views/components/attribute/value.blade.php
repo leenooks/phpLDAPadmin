483fbbd3e49f29d0a0b0618e20e65d3175216fd7
Deon George <deon@dege.au>
2025-12-25 15:01:12 +1100

    The RDN should not be editable directly

--- resources/views/components/attribute/value.blade.php
+++ resources/views/components/attribute/value.blade.php
@@ -1,6 +1,9 @@
 <div class="input-group has-validation">
 	<input type="text"
-		{{ $attributes->class(['is-invalid'=>($e=array_merge($errors->get($o->name_lc.'.'.$dotkey),$errors->get('_auto_value.'.$o->name_lc)))]) }}
+		{{ $attributes->class([
+			'is-invalid'=>($e=array_merge($errors->get($o->name_lc.'.'.$dotkey),$errors->get('_auto_value.'.$o->name_lc))),
+			'no-edit'=>$o->isRDN(),
+		]) }}
 		name="{{ $o->name_lc }}[{{ $attrtag }}][]"
 		value="{{ $value }}"
 		placeholder="{{ $value ?: '['.__('NEW').']' }}"
